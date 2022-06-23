ae516df5532792eb8f15b2bc002194956465b464
Abijeet <63050833+Abijeet-Ind@users.noreply.github.com>
2022-06-23 18:40:14 +0545

    Update navRoute.js

--- backend/routes/navRoute.js
+++ backend/routes/navRoute.js
@@ -2,16 +2,20 @@ const express = require("express");
 const router = express.Router();
 const navController = require("./../controllers/navController");
 
-router
-    .route("/how-we-work")
-    .get(navController.how_we_work);
-    
-router
-    .route("/event-listening")
-    .get(navController.event_listening);
+router.get("/", navController.home);
 
-router
-    .route("/contact")
-    .get(navController.contact);
+router.get("/how-we-work", navController.how_we_work);
+
+router.get("/event-listening", navController.event_listening);
+
+router.get("/contact", navController.contact);
+
+router.post("/register", navController.register);
+
+router.get("/event/:slug", navController.slugData);
+
+router.delete("/event/delete-event/:id", navController.eventDelete);
+
+router.patch("/event/updateEvent/:id", navController.updateData);
 
 module.exports = router;
